08e6a8dadf1cf1102a950565e1aa3613008d339f
Ganesh Gowda <ganeshabt180@gmail.com>
2026-06-23 22:21:03 +0530

    Added create post feature and some css

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
+let posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+app.get("/",(req,res)=>
+{
+    res.render("index",{
+        posts:posts
+    })
+
+})
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+app.post("/create",(req,res)=>{
+ const post={
+    id:Date.now(),
+    title:req.body.title,
+    content:req.body.content
+ };
+ posts.push(post);
+ res.redirect("/");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
