012f13c43661b4ea928b3fc0e341101bdf99dc9d
Ganesh Gowda <ganeshabt180@gmail.com>
2026-06-30 20:55:30 +0530

    Prepare project for deployment

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
-let posts = [];
+const PORT = process.env.PORT || 3000;let posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
