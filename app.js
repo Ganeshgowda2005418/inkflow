5aa7f3587653eea54cc1afa659e5b432a0bbd6aa
Ganesh Gowda <ganeshabt180@gmail.com>
2026-06-30 20:41:23 +0530

    Addde edit and delete function and improved ui

--- app.js
+++ app.js
@@ -28,8 +28,38 @@ app.post("/create",(req,res)=>{
  posts.push(post);
  res.redirect("/");
 });
+app.get("/edit/:id", (req, res) => {
+    const id = Number(req.params.id);
 
+    const post = posts.find(post => post.id === id);
+console.log(req.params.id);
+console.log(posts);
+console.log("Found post:", post);
 
+    res.render("edit", {
+        post: post
+    });
+});
+app.post("/edit/:id",(req,res)=>{
+    const id =Number(req.params.id);
+    const post=posts.find(post=>post.id===id);
+    if(!post){
+        return res.status(404).send("Post not found")
+    }
+
+    post.title = req.body.title;
+    post.content = req.body.content;
+
+    res.redirect("/");
+}
+);
+app.post("/delete/:id",(req,res)=>{
+    const id =Number(req.params.id);
+        posts = posts.filter(post => post.id !== id);
+
+    res.redirect("/");
+
+})
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
